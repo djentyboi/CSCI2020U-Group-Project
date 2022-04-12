f263da8d0ec84da5ac9c530a05cbd98c9b285b61
BraedenGibson <bgibson1338@gmail.com>
2022-04-12 02:01:01 -0400

    Added base layouts for the team select screen and the game scene.
    Nothing in Controller.java class yet, need to figure out how to implement that to reduce clutter.

--- src/sample/Main.java
+++ src/sample/Main.java
@@ -1,18 +1,39 @@
 package sample;
 
 import javafx.application.Application;
-import javafx.fxml.FXMLLoader;
-import javafx.scene.Parent;
+import javafx.scene.control.*;
 import javafx.scene.Scene;
+import javafx.scene.layout.BorderPane;
+import javafx.scene.layout.VBox;
 import javafx.stage.Stage;
 
 public class Main extends Application {
 
+    Scene teamSelectScene, gameScene;
+
     @Override
-    public void start(Stage primaryStage) throws Exception{
-        Parent root = FXMLLoader.load(getClass().getResource("sample.fxml"));
-        primaryStage.setTitle("Hello World");
-        primaryStage.setScene(new Scene(root, 300, 275));
+    public void start(final Stage primaryStage) throws Exception{
+
+        //Will figure out how to use controller class optimally to reduce clutter in main class
+
+        primaryStage.setTitle("CSCI2020U-Group-Project");
+
+        //Borderpane layout for team select screen. (experiment with other layouts possibly)
+        BorderPane teamSelectLayout = new BorderPane();
+        teamSelectScene = new Scene(teamSelectLayout, 600, 600);
+        Button button1 = new Button("Go to game scene");
+        button1.setOnAction(e -> primaryStage.setScene(gameScene));
+        teamSelectLayout.setCenter(button1);
+
+        //Temporary scene 2 layout (This is where game will take place)
+        VBox gameLayout = new VBox(20);
+        gameScene = new Scene(gameLayout, 600, 600);
+        Button button2 = new Button("Back to scene 1");
+        button2.setOnAction(e -> primaryStage.setScene(teamSelectScene));
+        gameLayout.getChildren().addAll(button2);
+
+        primaryStage.setResizable(false);
+        primaryStage.setScene(teamSelectScene);
         primaryStage.show();
     }
 
