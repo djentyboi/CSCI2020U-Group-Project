b5d94a7e47ed255b9f83bb1972aa10812add99fc
BraedenGibson <bgibson1338@gmail.com>
2022-04-13 01:43:46 -0400

    Base client/server connection

--- src/sample/Controller.java
+++ src/sample/Controller.java
@@ -1,6 +1,14 @@
 package sample;
 
-public class Controller {
+import javafx.fxml.Initializable;
+import javafx.scene.Parent;
 
+import java.awt.event.ActionEvent;
+import java.io.IOException;
+import java.net.ServerSocket;
+import java.net.URL;
+import java.util.ResourceBundle;
+
+public class Controller {
 
 }
--- src/sample/Main.java
+++ src/sample/Main.java
@@ -1,6 +1,7 @@
 package sample;
 
 import javafx.application.Application;
+import javafx.application.Platform;
 import javafx.scene.control.*;
 import javafx.scene.Scene;
 import javafx.scene.layout.BorderPane;
@@ -14,29 +15,29 @@ public class Main extends Application {
     @Override
     public void start(final Stage primaryStage) throws Exception{
 
-        //Will figure out how to use controller class optimally to reduce clutter in main class
 
-        primaryStage.setTitle("CSCI2020U-Group-Project");
-
-        //Borderpane layout for team select screen. (experiment with other layouts possibly)
-        BorderPane teamSelectLayout = new BorderPane();
-        teamSelectScene = new Scene(teamSelectLayout, 600, 600);
-        Button button1 = new Button("Go to game scene");
-        button1.setOnAction(e -> primaryStage.setScene(gameScene));
-        teamSelectLayout.setCenter(button1);
-
-        //Temporary scene 2 layout (This is where game will take place)
-        VBox gameLayout = new VBox(20);
-        gameScene = new Scene(gameLayout, 600, 600);
-        Button button2 = new Button("Back to scene 1");
-        button2.setOnAction(e -> primaryStage.setScene(teamSelectScene));
-        gameLayout.getChildren().addAll(button2);
-
-        primaryStage.setResizable(false);
-        primaryStage.setScene(teamSelectScene);
-        primaryStage.show();
+//        //Will figure out how to use controller class optimally to reduce clutter in main class
+//
+//        primaryStage.setTitle("CSCI2020U-Group-Project");
+//
+//        //Borderpane layout for team select screen. (experiment with other layouts possibly)
+//        BorderPane teamSelectLayout = new BorderPane();
+//        teamSelectScene = new Scene(teamSelectLayout, 600, 600);
+//        Button button1 = new Button("Go to game scene");
+//        button1.setOnAction(e -> primaryStage.setScene(gameScene));
+//        teamSelectLayout.setCenter(button1);
+//
+//        //Temporary scene 2 layout (This is where game will take place)
+//        VBox gameLayout = new VBox(20);
+//        gameScene = new Scene(gameLayout, 600, 600);
+//        Button button2 = new Button("Back to scene 1");
+//        button2.setOnAction(e -> primaryStage.setScene(teamSelectScene));
+//        gameLayout.getChildren().addAll(button2);
+//
+//        primaryStage.setResizable(false);
+//        primaryStage.setScene(teamSelectScene);
+////        primaryStage.show();
     }
-
 
     public static void main(String[] args) {
         launch(args);
