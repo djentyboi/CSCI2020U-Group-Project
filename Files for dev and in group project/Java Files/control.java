b0fc8e551c7745949be06afcd32216df960c4748
Riley Dunn <56401648+git-it-dunn@users.noreply.github.com>
2022-04-14 04:12:21 -0400

    Add files via upload

---
+++ Files for dev and in group project/Java Files/control.java
@@ -0,0 +1,15 @@
+package csci2020u.GroupProject;
+
+import javafx.fxml.Initializable;
+
+import java.net.URL;
+import java.util.ResourceBundle;
+
+public class control implements Initializable {
+    @Override
+    public void initialize(URL location, ResourceBundle resources) {
+
+    }
+    //master class that controls both javaFX files for client and server......maybe
+
+}
---
+++ Files for dev and in group project/Java Files/main.java
@@ -0,0 +1,11 @@
+package csci2020u.GroupProject;
+
+public class main {
+    public static void main(String[] arge){
+        //call both servers
+        //server.serverSide();
+        //client.clientSide();
+
+        //just calls the javaFX things
+    }
+}
